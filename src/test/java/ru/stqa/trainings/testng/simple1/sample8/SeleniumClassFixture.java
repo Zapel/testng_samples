a8c3f74bd67af876bd3887073172fe73ace24772
Zapel <zapel176@ukr.net>
2018-03-23 22:22:43 +0200

    test1
    конфигурационные файлы

---
+++ src/test/java/ru/stqa/trainings/testng/simple1/sample8/SeleniumClassFixture.java
@@ -0,0 +1,28 @@
+package ru.stqa.trainings.testng.simple1.sample8;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class SeleniumClassFixture {
+
+  protected WebDriver driver;
+
+  @BeforeClass
+  public void setUp() {
+    System.out.println("Starting a browser");
+    driver = new ChromeDriver();
+  }
+
+  @AfterClass
+  public void tearDown() {
+    System.out.println("Stopping the browser");
+    if (driver != null) {
+      driver.quit();
+    }
+  }
+
+}
